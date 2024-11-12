49477cdde1bc5a9ac131ac78d94ad9fe3ee84b99
hiepcode <nguyenhiep.190217@gmail.com>
2024-11-12 19:04:48 +0700

    First commit

--- AIO.ps1
+++ AIO.ps1
@@ -1,7 +1,7 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/nguyenhiep2805/AIO/main/AIO.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/nguyenhiep2805/AIO-Tool/refs/heads/main/AIO.cmd'
 
 $FilePath = "$env:TEMP\IAS.cmd"
 
